c3c3d0659a34dbae802114f46a130665258e8bfa
samyoga <bhattaraisamyoga@gmail.com>
2018-10-03 14:39:53 +0545

    Updating views and urls

--- bookreview/urls.py
+++ bookreview/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /bookreview/
     url(r'^$', views.index, name='index'),
+
+    #/bookreview/id/
+    url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- bookreview/views.py
+++ bookreview/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse ("hello")
+
+def detail(request, book_id):
+    return HttpResponse("<h2> details" + str(book_id) + "</h2>")
