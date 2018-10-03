2d921467d4e899bcdf3aa7f82fd5919fdc80ef2d
samyoga <bhattaraisamyoga@gmail.com>
2018-10-03 14:18:09 +0545

    filtering databse results

--- bookreview/models.py
+++ bookreview/models.py
@@ -6,10 +6,16 @@ class Book(models.Model):
     genre = models.CharField(max_length=100)
     book_cover = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.author + ' - ' + self.book_title
+
 class Movie(models.Model):
     casts = models.CharField(max_length=500)
     movie_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     movie_poster = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.casts + ' - ' + self.movie_title
 
 
