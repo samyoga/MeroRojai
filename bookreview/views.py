f31f83c3365bcd51b6eaa8baed10ae0a0566e796
samyoga <bhattaraisamyoga@gmail.com>
2018-10-03 17:02:52 +0545

    Connecting to the database

--- bookreview/views.py
+++ bookreview/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import HttpResponse
+from .models import Book, Movie
 
 def index(request):
-    return HttpResponse ("hello")
+    all_books = Book.objects.all()
+    html = ''
+    for book in all_books:
+        url = '/bookreview/' + str(book.id) + '/'
+        html += '<a href="' + url + '">' + book.book_title + '</a><br>'
+    return HttpResponse (html)
 
 def detail(request, book_id):
     return HttpResponse("<h2> details" + str(book_id) + "</h2>")
