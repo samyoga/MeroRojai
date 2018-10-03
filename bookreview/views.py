566df31b278c4d4b4288dd1b70e23753bb33d4f2
samyoga <bhattaraisamyoga@gmail.com>
2018-10-03 22:31:20 +0545

    render template shortcuts

--- bookreview/views.py
+++ bookreview/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Book, Movie
 
 def index(request):
     all_books = Book.objects.all()
-    template = loader.get_template('bookreview/index.html')
     context = {
         'all_books' : all_books
     }
-    return HttpResponse (template.render(context,request))
+    return render(request, 'bookreview/index.html', context)
 
 def detail(request, book_id):
     return HttpResponse("<h2> details" + str(book_id) + "</h2>")
