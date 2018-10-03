926f21ac1af9741e1223f519ec735ea8c6cf47da
samyoga <bhattaraisamyoga@gmail.com>
2018-10-03 22:44:28 +0545

    Raising a 404 HTTP error for id's that are not in database

--- bookreview/views.py
+++ bookreview/views.py
@@ -1,13 +1,16 @@
+from django.http import Http404
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from .models import Book, Movie
 
 def index(request):
     all_books = Book.objects.all()
-    context = {
-        'all_books' : all_books
-    }
+    context = { 'all_books' : all_books }
     return render(request, 'bookreview/index.html', context)
 
 def detail(request, book_id):
-    return HttpResponse("<h2> details" + str(book_id) + "</h2>")
+    try: 
+        book = Book.objects.get( pk = book_id )
+    except Book.DoesNotExist:
+        raise Http404(" Book does not exist ")
+    return render(request, 'bookreview/detail.html', {'book':book})
